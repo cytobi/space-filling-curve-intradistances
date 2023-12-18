0aec3b2df4bf2466c118b410c5adda54bd2c9cbf
cytobi <tobi81548@gmail.com>
2023-12-18 22:41:03 +0000

    peano

--- main.py
+++ main.py
@@ -39,6 +39,23 @@ class Z(Curve): # Z curve
     def get_matrix(self):
         return self.create(self.size)
     
+class Peano(Curve): # Peano curve
+    def create(self, iterations):
+        if iterations == 1:
+            return np.array([[2, 3, 8], [1, 4, 7], [0, 5, 6]])
+        else:
+            m = self.create(iterations-1)
+            mh = np.fliplr(m)
+            mv = np.flipud(m)
+            mvh = np.fliplr(mv)
+            m = np.block([[m+2*m.size, mv+3*m.size, m+8*m.size],
+                          [mh+1*m.size, mvh+4*m.size, mh+7*m.size],
+                          [m+0*m.size, mv+5*m.size, m+6*m.size]])
+            return m
+        
+    def get_matrix(self):
+        return self.create(self.size)
+    
         
 def calculate_average_distance(m):
     len = m.shape[0]
@@ -58,17 +75,21 @@ def calculate_average_distance(m):
 
 
 if __name__ == "__main__":
-    max_size = 128
+    max_size = 256
 
     lines_data = [(i, calculate_average_distance(Lines(i).get_matrix())) for i in range(2, max_size+1)]
     plt.plot(*zip(*lines_data), label="Lines", marker=".")
 
-    max_size_log = int(np.log2(max_size))
-    hilbert_data = [(2**i, calculate_average_distance(Hilbert(i).get_matrix())) for i in range(1, max_size_log+1)]
+    max_size_log2 = int(np.log2(max_size))
+    hilbert_data = [(2**i, calculate_average_distance(Hilbert(i).get_matrix())) for i in range(1, max_size_log2+1)]
     plt.plot(*zip(*hilbert_data), label="Hilbert", marker=".")
 
-    z_data = [(2**i, calculate_average_distance(Z(i).get_matrix())) for i in range(1, max_size_log+1)]
+    z_data = [(2**i, calculate_average_distance(Z(i).get_matrix())) for i in range(1, max_size_log2+1)]
     plt.plot(*zip(*z_data), label="Z", marker=".")
+
+    max_size_log3 = int(np.emath.logn(3, max_size))
+    peano_data = [(3**i, calculate_average_distance(Peano(i).get_matrix())) for i in range(1, max_size_log3+1)]
+    plt.plot(*zip(*peano_data), label="Peano", marker=".")
 
     plt.legend()
     plt.xlabel("Size")
